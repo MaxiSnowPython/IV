5d0b3b3df6c7bd4f4bc0ddf07337e5519c449ae6
maksym <m.boechko.xam@gmail.com>
2025-02-04 23:19:09 +0100

    g

--- myproj/main/views.py
+++ myproj/main/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<h4>Proverka</h4>")
+
+def about(request):
+    return HttpResponse("<h4>Strona pro nas</h4>")
